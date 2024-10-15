36064dd2113c681f9476711862865c551f5f1a5b
weaamabusiam <weam.abo99@gmail.com>
2024-10-16 02:53:52 +0300

    //הוספת רשימת נקודות שמירה

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+const port = 4326;
+
 const express = require('express');
 const app = express();
-const port = 3004;
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "public")));
+
+// רשימת נקודות שמירה
+let points = [
+    { pointName: "חדר מצלמות", location: "צפון" },
+    { pointName: "מחסן ראשי", location: "מערב" },
+    { pointName: "חדר שמירה", location: "דרום" },
+    { pointName: "בניין משרדים", location: "מזרח" },
+    { pointName: "שער ראשי", location: "מזרח" },
+    { pointName: "חניון אורחים", location: "מערב" }
+];
+
+app.get('/points', (req, res) => {
+    res.status(200).json(points);
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
